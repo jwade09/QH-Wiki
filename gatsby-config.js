9f20d2269c077035dbb980c2bb19b61060761d28
Jonathan Wade <jonathan.wade@Computer-5639.local>
2022-04-18 13:24:49 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: `QH Wiki`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "ifLQruTIoriR7UOQs2GXF08Uq56iJeKAFs-ed0PcbIk",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
