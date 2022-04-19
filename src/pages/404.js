304e7657c00262481b1dd53053fd9e1c6906d7c5
Jonathan Wade <jonathan.wade@quantum-health.com>
2022-04-19 13:27:20 -0400

    Updated home with links

--- src/pages/404.js
+++ src/pages/404.js
@@ -35,7 +35,7 @@ const NotFoundPage = () => {
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        we couldn’t find what you were looking for.
+        we couldn’t find what you were looking for. Good luck!
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,28 @@
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 // markup
 const IndexPage = () => {
+
+  const { allContentfulEmailTemplate } = useStaticQuery(
+    graphql`
+    {
+        allContentfulEmailTemplate {
+    nodes {
+      title
+      slug
+    }
+  }
+}
+  `)
+
   return (
     <main>
       <h1>Hello world!</h1>
+      {allContentfulEmailTemplate.nodes.map(node => (
+        <Link key={node.slug} to={`/email/${node.slug}`}>{node.title}</Link>
+      ))}
+      <p></p>
     </main>
   )
 }
